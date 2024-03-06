b662960aebe8690798edc09402f06b90581ee6cb
CarlosRRamos <carlosrafaelramos17@gmail.com>
2024-03-06 02:53:35 -0300

    trabajo en clase

---
+++ src/app.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+const PORT = 3000;
+
+app.listen(PORT, () => console.log("App listening to port: " + PORT));
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/vista/home.html");
+});
+
+app.get("/usuarios/:usuarioId", (req, res) => {
+  const usuarioId = req.params.usuarioId;
+  res.sendFile(__dirname + "/vista/usuario" + usuarioId + ".html");
+});
+
+app.get("/login", (req, res) => {
+  res.sendFile(__dirname + "/vista/login.html");
+});
+
+app.get("/registro", (req, res) => {
+  res.sendFile(__dirname + "/vista/registro.html");
+});
